aa533c22d49b67b81c70dbf341d0a0d3b9eb269e
Khuram <khuram.saggu@gmail.com>
2024-07-28 11:41:45 +0500

    comments added in comparisiom.go file

--- comparision.go
+++ comparision.go
@@ -7,15 +7,15 @@ import (
 func main() {
 	var a int = 12
 	var b int = 12
-	fmt.Println(a <= b)
+	fmt.Println(a <= b) // True a = 12. b = 12
 
 	a = 20
-	fmt.Println(a <= b)
+	fmt.Println(a <= b) // False a = 20 , b = 12
 
 	b = 100
-	fmt.Println(a <= b)
+	fmt.Println(a <= b) // True a = 20 , b = 100
 
 	c := 0
-	fmt.Println(a <= c)
+	fmt.Println(a <= c) // False a = 20 , c = 0
 
 }
