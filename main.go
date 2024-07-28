a06b6070e1eccecfe4b254441b9fd546d093c24f
Khuram <khuram.saggu@gmail.com>
2024-07-28 10:23:57 +0500

    initial push

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// this is a comment
+
+func main() {
+
+	//var name string = "Kodekloud"
+	name := "KodeKloud" // a variable can also be initialized this way implicitly
+
+	fmt.Printf("Salam and welcome, to %v \n", name) //%v is a placeholder for string data type
+	fmt.Printf("Salam %v and welcome", name)
+	//fmt.Print("Salam and welcome, to ", name)
+
+}
+
+/*
+package main
+
+import "fmt"
+
+// this is a comment
+
+func main() {
+
+	fmt.Println("Salam !!!")
+}
+*/
---
+++ string_placeholders_variable_decliration.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var name string = "Khuram" //its explicit way define a varible
+	//var marks int = 98
+	name := "Khuram" //its shortes way to decalre a verable implicitly
+	marks := 98
+	//fmt.Printf("Salam %v", name)
+	//fmt.Printf("You secured %d marks", marks)
+	fmt.Printf("Salam %v you have secured %d marks out of 100", name, marks)
+}
