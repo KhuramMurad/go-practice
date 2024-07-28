2e2f9b688db37e59c047fa8cb06e9045f2ae9803
Khuram <khuram.saggu@gmail.com>
2024-07-28 12:08:42 +0500

    arithmatci operators file is added

---
+++ operators.go
@@ -0,0 +1,40 @@
+/*
+package main
+
+import "fmt"
+
+	func main() {
+	        var a string = "one"
+	        var b int = 2
+	        fmt.Print(a + b)
+	}
+
+package main
+
+import "fmt"
+
+	func main() {
+		var a float64 = 5.9
+		var b int = 2
+		fmt.Print(a + b)
+	}
+
+package main
+
+import "fmt"
+
+	func main() {
+		var a float64 = 5.9
+		a++
+		fmt.Print(a)
+	}
+*/
+package main
+
+import "fmt"
+
+func main() {
+	var a, b float64 = 24.4, 3.0
+	fmt.Println(a / b)
+	fmt.Println(int(a) % int(b))
+}
